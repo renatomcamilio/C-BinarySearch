2f0485ab5f59b10f7840766908a2753bff8c74e6
Renato Camilio <renatomcamilio@gmail.com>
2015-01-18 16:16:07 -0800

    Part 1 Write a function that will take a sorted array and a target number that will return the index of where it found the target or -1 if it cannot find it.

--- BinarySearch/main.c
+++ BinarySearch/main.c
@@ -7,9 +7,33 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+int binarySearch(int array[], int value, int min, int max) {
+    if (max < min) {
+        return -1;
+    }
+    
+    int middleIndex = (min + max) / 2;
+
+    if (array[middleIndex] > value) {
+        return binarySearch(array, value, min, middleIndex - 1);
+    } else if (array[middleIndex] < value) {
+        return binarySearch(array, value, middleIndex + 1, max);
+    } else {
+        return middleIndex;
+    }
+}
+
+
+
+int main(int argc, char * argv[]) {
+    int numbers[] = { 1, 2, 2, 3, 4, 4, 4 };
+    int numbersSize = sizeof(numbers) / sizeof(int);
+
+    int result = binarySearch(numbers, 4, 0, numbersSize - 1);
+    printf("found at index %i\n", result);
+
     return 0;
 }
---
+++ Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  BinarySearch
+//
+//  Created by Renato Camilio on 1/13/15.
+//  Copyright (c) 2015 Renato Camilio. All rights reserved.
+//
+
+#ifndef __BinarySearch__Test__
+#define __BinarySearch__Test__
+
+#include <stdio.h>
+
+#endif /* defined(__BinarySearch__Test__) */
